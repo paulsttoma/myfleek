9f0764b601b826db891022e52b22b2129c3ad8db
Dev <pauls@Paulss-MacBook-Pro.local>
2021-11-10 16:42:26 -0500

    new

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    
         <p>
           PROJECTS COMING SOON!
         </p>
-      </header>
-    </div>
+      
   );
 }
 
