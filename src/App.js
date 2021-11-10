c6e15025ec07cdabac252b4b59f5e8990b39a145
Dev <pauls@Paulss-MacBook-Pro.local>
2021-11-10 16:35:57 -0500

    nov 10th

--- src/App.js
+++ src/App.js
@@ -7,12 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Big P and Little P Crypto projects. 
-          I gotta grind for midter
+          PROJECTS COMING SOON!
         </p>
-        <a>
-          I developed this using react js and fleek
-        </a>
       </header>
     </div>
   );
